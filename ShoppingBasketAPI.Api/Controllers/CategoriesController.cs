d5ee427d28d92642cf75002c8b36ff7fae42b710
shuvabiswas12 <shuvabiswas12@gmail.com>
2024-06-15 22:59:59 +0600

    added functionality of getting all category and get single category by id.

---
+++ ShoppingBasketAPI.Api/Controllers/CategoriesController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ShoppingBasketAPI.Services.IServices;
+
+namespace ShoppingBasketAPI.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoriesController : ControllerBase
+    {
+        private readonly ICategoryServices _categoryServices;
+
+        public CategoriesController(ICategoryServices categoryServices)
+        {
+            this._categoryServices = categoryServices;
+        }
+
+        /***
+         * Get all category.
+         */
+
+        [HttpGet("")]
+        public async Task<IActionResult> GetAll()
+        {
+            var result = await _categoryServices.GetAllCategories();
+            return Ok(result);
+        }
+
+        /***
+         * Get a single category.
+         */
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetById(string id)
+        {
+            var result = await _categoryServices.GetCategoryById(id);
+            if (result == null)
+            {
+                return NotFound();
+            }
+            return Ok(result);
+        }
+    }
+}
