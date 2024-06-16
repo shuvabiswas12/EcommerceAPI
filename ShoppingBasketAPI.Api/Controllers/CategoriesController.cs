76f08754005e32913d76c00c007b355f75c1a12e
shuvabiswas12 <shuvabiswas12@gmail.com>
2024-06-16 12:03:11 +0600

    Category controller just done!

--- ShoppingBasketAPI.Api/Controllers/CategoriesController.cs
+++ ShoppingBasketAPI.Api/Controllers/CategoriesController.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using ShoppingBasketAPI.Domain;
+using ShoppingBasketAPI.DTOs;
 using ShoppingBasketAPI.Services.IServices;
 
 namespace ShoppingBasketAPI.Api.Controllers
@@ -27,6 +29,43 @@ namespace ShoppingBasketAPI.Api.Controllers
         }
 
         /***
+         * Create category
+         */
+
+        [HttpPost("")]
+        public async Task<IActionResult> CreateCategory(CategoryCreateRequestDTO createCategory)
+        {
+            if (createCategory.Name.Trim().Length == 0)
+            {
+                return BadRequest(new { error = "Must be filled with a category name." });
+            }
+            Category category = await _categoryServices.CreateCategory(new Category { Name = createCategory.Name.Trim() });
+            return Ok(new { category });
+        }
+
+        /***
+         * Category update
+         */
+
+        [HttpPut("")]
+        public async Task<IActionResult> UpdateCategory(CategoryUpdateRequestDTO categoryToUpdate)
+        {
+            if (categoryToUpdate.Name.Trim().Length == 0 || categoryToUpdate.Id.Trim().Length == 0)
+            {
+                return BadRequest(new { error = "Data is missing!" });
+            }
+            try
+            {
+                Category updatedCategory = await _categoryServices.UpdateCategory(categoryToUpdate.Id.Trim(), categoryToUpdate.Name.Trim());
+                return Ok(updatedCategory);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        /***
          * Get a single category.
          */
 
@@ -39,6 +78,28 @@ namespace ShoppingBasketAPI.Api.Controllers
                 return NotFound();
             }
             return Ok(result);
+        }
+
+        /***
+         * Delete category
+         */
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteCategory(string id)
+        {
+            if (id.Trim().Length == 0)
+            {
+                return BadRequest(new { error = "missing category id." });
+            }
+            try
+            {
+                await _categoryServices.DeleteCategory(id);
+                return Ok(new { message = "Deleted." });
+            }
+            catch (Exception ex)
+            {
+                return NotFound(ex.Message);
+            }
         }
     }
 }
---
+++ ShoppingBasketAPI.DTOs/CategoryUpdateRequestDTO.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShoppingBasketAPI.DTOs
+{
+    public class CategoryUpdateRequestDTO
+    {
+        public string Name { get; set; } = null!;
+        public string Id { get; set; } = null!;
+    }
+}
