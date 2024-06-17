f9251dc05caf515762d0751e2e6a0c1f3bb868c2
shuvabiswas12 <shuvabiswas12@gmail.com>
2024-06-18 00:02:13 +0600

    added product controller with get all product, get product by id, create product action method. added one create request product dto.

--- ShoppingBasketAPI.Api/Controllers/ProductsController.cs
+++ ShoppingBasketAPI.Api/Controllers/ProductsController.cs
@@ -1,6 +1,11 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using ShoppingBasketAPI.Domain;
+using ShoppingBasketAPI.DTOs;
 using ShoppingBasketAPI.Services.IServices;
+using ShoppingBasketAPI.Utilities;
+using ShoppingBasketAPI.Utilities.Validation;
 
 namespace ShoppingBasketAPI.Api.Controllers
 {
@@ -9,10 +14,12 @@ namespace ShoppingBasketAPI.Api.Controllers
     public class ProductsController : ControllerBase
     {
         private IProductServices _productService;
+        private ILogger<ProductsController> _logger;
 
-        public ProductsController(IProductServices productService)
+        public ProductsController(IProductServices productService, ILogger<ProductsController> logger)
         {
             _productService = productService;
+            _logger = logger;
         }
 
         /***
@@ -22,8 +29,16 @@ namespace ShoppingBasketAPI.Api.Controllers
         [HttpGet("")]
         public async Task<IActionResult> GetAllProducts()
         {
-            var productsResult = await _productService.GetAllProduct();
-            return Ok(productsResult);
+            try
+            {
+                var productsResult = await _productService.GetAllProduct();
+                return Ok(productsResult);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "An error occured while getting all product.");
+                return StatusCode(500, new { Error = GlobalErrorMessage.ErrorMessage });
+            }
         }
 
         /***
@@ -33,8 +48,56 @@ namespace ShoppingBasketAPI.Api.Controllers
         [HttpGet("id")]
         public async Task<IActionResult> GetProductsById(string id)
         {
-            var productResult = await _productService.GetProductById(id);
-            return Ok(productResult);
+            try
+            {
+                var productResult = await _productService.GetProductById(id);
+                return Ok(productResult);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "An error occured while getting product by id.");
+                return StatusCode(500, new { Error = GlobalErrorMessage.ErrorMessage });
+            }
+        }
+
+        /***
+         * Create product.
+         */
+
+        [HttpPost("")]
+        public async Task<IActionResult> CreateProduct(ProductCreateRequestDTO productDto)
+        {
+            var modelState = ModelValidator.ValidateModel(productDto);
+            if (!modelState.IsValid)
+            {
+                var errors = modelState
+                    .Where(ms => ms.Value!.Errors.Any())
+                    .ToDictionary(
+                        kvp => kvp.Key,
+                        kvp => kvp.Value!.Errors.Select(e => e.ErrorMessage).ToArray()
+                    );
+
+                return BadRequest(new { Errors = errors });
+            }
+
+            try
+            {
+                var newProduct = new Product
+                {
+                    Name = productDto.Name.Trim(),
+                    Description = productDto.Description.Trim(),
+                    Price = productDto.Price,
+                    Images = productDto.ImageUrls.Select(url => new Image { ImageUrl = url }).ToList(),
+                };
+
+                newProduct = await _productService.CreateProduct(newProduct);
+                return Ok(newProduct);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "An error occured while creating new product.");
+                return StatusCode(StatusCodes.Status500InternalServerError, new { Error = GlobalErrorMessage.ErrorMessage });
+            }
         }
     }
 }
---
+++ ShoppingBasketAPI.DTOs/ProductCreateRequestDTO.cs
@@ -0,0 +1,19 @@
+﻿using ShoppingBasketAPI.Utilities.Validation.CustomAttributes;
+using ShoppingBasketAPI.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShoppingBasketAPI.DTOs
+{
+    public class ProductCreateRequestDTO
+    {
+        [NotEmpty(errorMessage: "Name field is required.")] public string Name { get; set; } = null!;
+        [NotEmpty(errorMessage: "Description field is required.")] public string Description { get; set; } = null!;
+        [GreaterThan(0, "Price field must be greater than zero.")] public decimal Price { get; set; }
+        [MinimumOneImageUrl("At least one image URL is required.")] public ICollection<string> ImageUrls { get; set; } = new List<string>();
+    }
+}
