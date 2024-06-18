95ab9ac915f380d589ead766c4132c43b8d4366e
shuvabiswas12 <shuvabiswas12@gmail.com>
2024-06-18 13:50:17 +0600

    Added one global delete message.

---
+++ ShoppingBasketAPI.Utilities/GlobalDeleteMessage.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShoppingBasketAPI.Utilities
+{
+    public static class GlobalDeleteMessage
+    {
+        public static string DeleteMessage { get; private set; } = "Deleted.";
+    }
+}
