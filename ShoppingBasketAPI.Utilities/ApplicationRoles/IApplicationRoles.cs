7466edf15903e5294dd69ebbcc129cad9a73942c
shuvabiswas12 <shuvabiswas12@gmail.com>
2024-06-20 14:07:55 +0600

    added application roles.

---
+++ ShoppingBasketAPI.Utilities/ApplicationRoles/IApplicationRoles.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShoppingBasketAPI.Utilities.ApplicationRoles
+{
+    public interface IApplicationRoles
+    {
+        public static readonly string ADMIN = "Admin";
+        public static readonly string EMPLOYEE = "Employee";
+    }
+}
