3e48b101ecc643481bfc8da2dc61dcf4f18ff341
shuvabiswas12 <shuvabiswas12@gmail.com>
2024-06-20 14:20:24 +0600

    Added two new DTOs for Rregistration.

---
+++ ShoppingBasketAPI.DTOs/RegistrationResponseDTO.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShoppingBasketAPI.DTOs
+{
+    public class RegistrationResponseDTO
+    {
+        public required string Message { get; set; }
+        public required bool IsSuccess { get; set; }
+        public required User User { get; set; }
+    }
+    public class User
+    {
+        public required string Id { get; set; }
+    }
+}
