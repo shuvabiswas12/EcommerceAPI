0857b4e838cce3cc6f52410317b18e688897a716
shuvabiswas12 <shuvabiswas12@gmail.com>
2024-06-21 14:22:54 +0600

    modified internal structure.

--- ShoppingBasketAPI.Utilities/Exceptions/NotFoundException.cs
+++ ShoppingBasketAPI.Utilities/Exceptions/NotFoundException.cs
@@ -17,13 +17,5 @@ namespace ShoppingBasketAPI.Utilities.Exceptions
         public NotFoundException(string? message) : base(message)
         {
         }
-
-        public NotFoundException(string? message, Exception? innerException) : base(message, innerException)
-        {
-        }
-
-        protected NotFoundException(SerializationInfo info, StreamingContext context) : base(info, context)
-        {
-        }
     }
 }
