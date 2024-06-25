294fe97a5508881fcc2e7268876ee651390d8c0b
shuvabiswas12 <shuvabiswas12@gmail.com>
2024-06-26 02:02:23 +0600

    added some more dto and one more exception class.

---
+++ ShoppingBasketAPI.DTOs/FeaturedProductRequestDTO.cs
@@ -0,0 +1,15 @@
+﻿using ShoppingBasketAPI.Utilities.Validation.CustomAttributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShoppingBasketAPI.DTOs
+{
+    public class FeaturedProductRequestDTO
+    {
+        [NotEmpty(errorMessage: "Product id is required")]
+        public string Id { get; set; } = null!;
+    }
+}
