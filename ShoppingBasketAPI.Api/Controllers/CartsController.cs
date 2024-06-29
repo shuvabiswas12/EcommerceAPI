a60d82acb3cf10c0e0d41872db5fadf4caa900c9
shuvabiswas12 <shuvabiswas12@gmail.com>
2024-06-29 15:38:38 +0600

    some xml comments are modified.

--- ShoppingBasketAPI.Api/Controllers/CartsController.cs
+++ ShoppingBasketAPI.Api/Controllers/CartsController.cs
@@ -68,17 +68,16 @@ namespace ShoppingBasketAPI.Api.Controllers
         /// Adds a product to the shopping cart or updates the product quantity in the cart.
         /// </summary>
         /// <param name="shoppingCartCreateRequestDTO">
-        /// A <see cref="ShoppingCartCreateRequestDTO"/> containing the details of the product 
+        /// Containing the details of the product 
         /// to be added or updated in the shopping cart.
         /// </param>
         /// <returns>
-        /// An <see cref="IActionResult"/> indicating the result of the operation.
+        /// An indicating the result of the operation.
         /// </returns>
         /// <response code="200">Success - product added or updated in the cart.</response>
         /// <response code="400">Bad Request - invalid product details.</response>
         /// <response code="401">Unauthorized - API key is missing or invalid.</response>
         /// <response code="500">Internal Server Error - an error occurred while processing the request.</response>
-        /// <exception cref="NotImplementedException">Thrown when the method is not implemented.</exception>
         [HttpPost]
         [ApiKeyRequired, Authorize(Roles = "Web_User")]
         public Task<IActionResult> AddProductIntoCart([FromBody] ShoppingCartCreateRequestDTO shoppingCartCreateRequestDTO)
@@ -90,7 +89,7 @@ namespace ShoppingBasketAPI.Api.Controllers
         /// </summary>
         /// <param name="products">A list of product IDs to be removed from the cart.</param>
         /// <returns>
-        /// An <see cref="IActionResult"/> indicating the result of the operation.
+        /// An indicating the result of the operation.
         /// </returns>
         /// <response code="200">Success - products removed from the cart.</response>
         /// <response code="400">Bad Request - invalid product IDs.</response>
