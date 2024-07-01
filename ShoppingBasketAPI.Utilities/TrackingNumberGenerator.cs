20a4f98bc6a475c84794f91afdeaf80e1a90c600
shuvabiswas12 <shuvabiswas12@gmail.com>
2024-07-02 00:36:53 +0600

    added a tracking number generator class.

---
+++ ShoppingBasketAPI.Utilities/TrackingNumberGenerator.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShoppingBasketAPI.Utilities
+{
+    public class TrackingNumberGenerator
+    {
+        private static readonly Random _random = new Random();
+
+        public static string GenerateTrackingNumber()
+        {
+            // Generate a random number
+            int randomNumber = _random.Next(1000, 9999);
+
+            // Combine the current date and time with the random number
+            string trackingNumber = $"{DateTime.UtcNow:yyyyMMddHHmmss}-{randomNumber}";
+
+            return trackingNumber;
+        }
+    }
+}
