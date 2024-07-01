c43f6ba3e00a6e4fbcbc35ada00e59c758072ef7
shuvabiswas12 <shuvabiswas12@gmail.com>
2024-07-01 11:16:04 +0600

    added some computed property.

--- ShoppingBasketAPI.Domain/Product.cs
+++ ShoppingBasketAPI.Domain/Product.cs
@@ -5,6 +5,7 @@ using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 
 namespace ShoppingBasketAPI.Domain
@@ -19,13 +20,21 @@ namespace ShoppingBasketAPI.Domain
         [Required, Precision(10, 2)] public decimal Price { get; set; }
         public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
 
+        // Computed property.
+        [NotMapped]
+        public bool IsFeatured => FeaturedProduct != null;
+
+        // Computed property.
+        [NotMapped]
+        public double DiscountRate => Discount != null ? Discount.DiscountRate : 0.0;
+
         // One to many relationship
         // Navigation property
         public ICollection<Image> Images { get; set; } = new List<Image>();
 
         // Navigation properties
-        public Discount Discount { get; set; } = null!;
-        public FeaturedProduct FeaturedProduct { get; set; } = null!;
-        public ProductCategory ProductCategory { get; set; } = null!;
+        [JsonIgnore] public Discount Discount { get; set; } = null!;
+        [JsonIgnore] public FeaturedProduct FeaturedProduct { get; set; } = null!;
+        [JsonIgnore] public ProductCategory ProductCategory { get; set; } = null!;
     }
 }
