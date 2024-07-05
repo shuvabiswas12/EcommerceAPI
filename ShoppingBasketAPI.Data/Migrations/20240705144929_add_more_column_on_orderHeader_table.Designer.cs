6e5a70300fe210c0d85a303de146230a1b6efccd
shuvabiswas12 <shuvabiswas12@gmail.com>
2024-07-05 20:52:05 +0600

    added some more columns on order header table.

--- ShoppingBasketAPI.Data/Migrations/20240705144929_add_more_column_on_orderHeader_table.Designer.cs
+++ ShoppingBasketAPI.Data/Migrations/20240705144929_add_more_column_on_orderHeader_table.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using ShoppingBasketAPI.Data;
 
@@ -11,9 +12,11 @@ using ShoppingBasketAPI.Data;
 namespace ShoppingBasketAPI.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240705144929_add_more_column_on_orderHeader_table")]
+    partial class add_more_column_on_orderHeader_table
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
