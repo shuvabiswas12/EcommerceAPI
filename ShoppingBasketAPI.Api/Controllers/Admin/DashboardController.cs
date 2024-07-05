d43ff3d19f2249e1d6d6d82368f90eb2444955b0
shuvabiswas12 <shuvabiswas12@gmail.com>
2024-07-05 23:16:36 +0600

    Dashboard functionality is started to implement.

---
+++ ShoppingBasketAPI.Api/Controllers/Admin/DashboardController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ShoppingBasketAPI.Utilities.Exceptions.Handler;
+
+namespace ShoppingBasketAPI.Api.Controllers.Admin
+{
+    /// <summary>
+    /// Controller for managing dashboard-related operations.
+    /// </summary>
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DashboardController : ControllerBase
+    {
+        private readonly ExceptionHandler<DashboardController> _exceptionHandler;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="DashboardController"/> class.
+        /// </summary>
+        /// <param name="exceptionHandler">The exception handler specific to the <see cref="DashboardController"/>.</param>
+        public DashboardController(ExceptionHandler<DashboardController> exceptionHandler)
+        {
+            _exceptionHandler = exceptionHandler;
+        }
+    }
+}
