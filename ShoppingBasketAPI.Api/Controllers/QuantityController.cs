6e244d68d2d02fe38d552e52164240156c7b06f1
shuvabiswas12 <shuvabiswas12@gmail.com>
2024-07-09 11:32:55 +0600

    added empty controller and empty services for quantity handling.

---
+++ ShoppingBasketAPI.Api/Controllers/QuantityController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ShoppingBasketAPI.Api.Controllers
+{
+    /// <summary>
+    /// This controller is responsible for managing current availability of any products.
+    /// </summary>
+    [Route("api/[controller]")]
+    [ApiController]
+    public class QuantityController : ControllerBase
+    {
+    }
+}
---
+++ ShoppingBasketAPI.Services/Services/QuantityServices.cs
@@ -0,0 +1,14 @@
+﻿using ShoppingBasketAPI.Services.IServices;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShoppingBasketAPI.Services.Services
+{
+    public class QuantityServices : IQuantityServices
+    {
+
+    }
+}
