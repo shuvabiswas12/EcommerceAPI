7b9fddf28e87ca028e5ac5a620de38f5a83454b4
shuvabiswas12 <shuvabiswas12@gmail.com>
2024-08-01 12:01:54 +0600

    added ose user extension class.

---
+++ ShoppingBasketAPI.Utilities/UserExtension.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShoppingBasketAPI.Utilities
+{
+    public static class UserExtension
+    {
+        /// <summary>
+        /// Get User id of any authenticated user.
+        /// </summary>
+        /// <param name="user">Takes one parameter as user which is a object of ClaimsPrinciple.</param>
+        /// <returns>It Returns authenticated User's Id. It not authenticated it returns empty string.</returns>
+        public static string GetUserId(this ClaimsPrincipal user)
+        {
+            if (user.Identity?.IsAuthenticated == true)
+            {
+                return (user.Identity as ClaimsIdentity)?.FindFirst(ClaimTypes.Actor)?.Value!;
+            }
+            return string.Empty;
+        }
+    }
+}
