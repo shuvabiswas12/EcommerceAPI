947fdf83b708146e69a6fe90fb95c234119659fc
shuvabiswas12 <shuvabiswas12@gmail.com>
2025-05-02 11:30:20 +0600

    Add - Added a class for getting Timestamp.

---
+++ EcommerceAPI.Utilities/Timestamps.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EcommerceAPI.Utilities
+{
+    public class Timestamps
+    {
+        /// <summary>
+        /// Takes a DateTime object and returns a UnixTimeSeconds as Timestamp.
+        /// </summary>
+        public static long GetTimestamp(DateTime dateTime)
+        {
+            return ((DateTimeOffset)dateTime).ToUnixTimeSeconds();
+        }
+    }
+}
